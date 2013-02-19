cd06059138a111777534682b5f4fed290c36abe6
Nick Pelone <npelone@unca.edu>
2013-02-19 18:39:55 -0500

    Major overhaul: adds unstyled ten tweets at a time on the page, fades out the tenth tweet once it exists, continues to prepend a new tweet to the top of the page

---
+++ script.js
@@ -0,0 +1,8 @@
+var twitter = new ctwitter.CTwitter();
+twitter.stream("statuses/filter", { lang:"en", track:["Android"] }, function (stream) {
+  stream.on("data", function (tweet) {
+    var counter = 0;
+    $(".tweets").prepend('<p class="tweet">' + tweet.text);
+    $('p').eq(10).hide(400);
+  });
+});
