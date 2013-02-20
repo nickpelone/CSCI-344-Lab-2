45d1cf895fe06624035209f8d2be93e60f57068e
Nick Pelone <npelone@unca.edu>
2013-02-19 19:08:25 -0500

    Centers text, adjusts fontsize, some other random things

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var twitter = new ctwitter.CTwitter();
 twitter.stream("statuses/filter", { lang:"en", track:["Android"] }, function (stream) {
   stream.on("data", function (tweet) {
     var counter = 0;
-    $(".tweets").prepend('<p class="tweet">' + tweet.text);
+    $(".tweets").prepend('<p class="tweet">'+ tweet.text);
     $('p').eq(10).hide(400);
   });
 });
